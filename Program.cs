1bbd71bb229463371a2626eee95cc6930de98111
Juan Diego Guevara Esquivel <juandiego.guevaraesquivel@gmail.com>
2026-04-24 15:25:49 -0600

    DB context y  servicios
    Agregue RestaurantDbTestContext y sus DbSets, y cambios en Program.cs

--- Program.cs
+++ Program.cs
@@ -1,10 +1,32 @@
+using ProyectoIProgra2.Servicios;
+using ProyectoIProgra2.Servicios.Interfaces;
+using ProyectoIProgra2.RestaurantDbContext;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 
-builder.Services.AddControllers();
+builder.Servicios.AddScoped<RestaurantDbTestContext>();
+
+builder.Servicios.AddScoped<IBloqueoMesaServicio, BloqueoMesaServicio>();
+
+builder.Servicios.AddScoped<IClienteServicio, ClienteServicio>();
+
+builder.Servicios.AddScoped<IEstadoDeReservaServicio, EstadoDeReservaServicio>();
+
+builder.Servicios.AddScoped<IListaDeEsperaServicio, ListaDeEsperaServicio>();
+
+builder.Servicios.AddScoped<IMesaServicio, MesaServicio>();
+
+builder.Servicios.AddScoped<IReservaServicio, ReservaServicio>();
+
+builder.Servicios.AddScoped<ITurnoServicio, TurnoServicio>();
+
+builder.Servicios.AddScoped<IZonaServicio, ZonaServicio>();
+
+builder.Servicios.AddControllers();
 // Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
-builder.Services.AddOpenApi();
+builder.Servicios.AddOpenApi();
 
 var app = builder.Build();
 
