c970801f3d16614238f4cdfefe1a391a77597baa
Juan Diego Guevara Esquivel <juandiego.guevaraesquivel@gmail.com>
2026-04-24 14:26:31 -0600

    Update Cliente.cs

--- Entidades/Cliente.cs
+++ Entidades/Cliente.cs
@@ -4,9 +4,7 @@
     {
         public int Id { get; set; }
         public int DNI { get; set; }
-        public int Age { get; set; }
-        public int Tel { get; set; }
-        public string Email { get; set; }
+
         public string Name { get; set; }
         public string LastName { get; set; }
     }
