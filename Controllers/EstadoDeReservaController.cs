7f372b6d857a295e2476059e3e702b03d692c85a
Leo-Zs <ashly.zuniga.salas@est.una.ac.cr>
2026-04-24 22:04:22 -0600

    Controllers
    1

---
+++ Controllers/EstadoDeReservaController.cs
@@ -0,0 +1,43 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace ProyectoIProgra2.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class EstadoDeReservaController : ControllerBase
+    {
+        // GET: api/<EstadoDeReservaController>
+        [HttpGet]
+        public IEnumerable<string> Get()
+        {
+            return new string[] { "value1", "value2" };
+        }
+
+        // GET api/<EstadoDeReservaController>/5
+        [HttpGet("{id}")]
+        public string Get(int id)
+        {
+            return "value";
+        }
+
+        // POST api/<EstadoDeReservaController>
+        [HttpPost]
+        public void Post([FromBody] string value)
+        {
+        }
+
+        // PUT api/<EstadoDeReservaController>/5
+        [HttpPut("{id}")]
+        public void Put(int id, [FromBody] string value)
+        {
+        }
+
+        // DELETE api/<EstadoDeReservaController>/5
+        [HttpDelete("{id}")]
+        public void Delete(int id)
+        {
+        }
+    }
+}
--- Servicios/Interfaces/Prueba.cs
+++
@@ -1,6 +0,0 @@
-﻿namespace ProyectoIProgra2.Servicios.Interfaces
-{
-    public interface Prueba
-    {
-    }
-}
