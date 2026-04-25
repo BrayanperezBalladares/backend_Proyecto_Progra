c96de8679d56c6f1e41f7b727af28364a2d95dd2
Nahum Briceño Araya <nahum.devving@gmail.com>
2026-04-24 19:06:30 -0600

    Agregado el MyDbContext.cs
    Ya agregué el MyDbContext.cs en una nueva carpeta "Data".
    
    Para conectar las 8 entidades.

---
+++ Data/MyDbContext.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.EntityFrameworkCore;
+using ProyectoProgra2.Entidades;
+
+namespace ProyectoProgra2.Data
+{
+
+    public class MyDbContext : DbContext
+    {
+        public MyDbContext(DbContextOptions<MyDbContext> options) : base(options)
+        {
+        }
+
+        public DbSet<Cliente> Clientes { get; set; }
+        public DbSet<Mesa> Mesas { get; set; }
+        public DbSet<Reserva> Reservas { get; set; }
+        public DbSet<Zona> Zonas { get; set; }
+        public DbSet<Turno> Turnos { get; set; }
+        public DbSet<EstadoDeReserva> EstadosReserva { get; set; }
+        public DbSet<BloqueoMesa> BloqueosMesas { get; set; }
+        public DbSet<ListaDeEspera> ListasEspera { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+        
+        }
+    }
+}
